eb8928117ddc7700855f7c3bfadd4a2130849ed9
Mike Lenzen <m.lenzen@gmail.com>
2015-01-20 17:26:27 -0600

    Clean out all content.

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
 from os.path import dirname, join
-import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
-
-class PyTest(TestCommand):
-	def finalize_options(self):
-		TestCommand.finalize_options(self)
-		self.test_args = ['tests']
-		self.test_suite = True
-
-	def run_tests(self):
-		import pytest
-		errcode = pytest.main(self.test_args)
-		sys.exit(errcode)
 
 setup(
 		name='data-structures',
-		packages=['collections_extended'],
+		packages=[],
 		version='0.1.4',
 		description=(
 			'Extra Python Data Structures - bags (multisets) and setlists (ordered'
@@ -48,8 +35,6 @@ setup(
 			'Topic :: Software Development :: Libraries :: Python Modules',
 			],
 		long_description = open(join(dirname(__file__), 'README.rst')).read(),
-		install_requires=['setuptools'],
-		tests_require=['pytest'],
-		package_data={'': ['README.rst', 'LICENSE', 'CONTRIBUTING.rst']},
-		cmdclass={'test': PyTest},
+		install_requires=['setuptools', 'collections_extended'],
+		package_data={'': ['README.rst']},
 		)
--- tasks.py
+++ tasks.py
@@ -7,18 +7,6 @@ from invoke.util import log
 
 
 @task
-def test():
-	"""test - run the test runner."""
-	run('python setup.py test', pty=True)
-
-
-@task(name='test-all')
-def testall():
-	"""test-all - run tests on every Python version with tox."""
-	run('tox')
-
-
-@task
 def clean():
 	"""clean - remove build artifacts."""
 	run('rm -rf build/')
@@ -32,23 +20,6 @@ def clean():
 	log.info('cleaned up')
 
 
-@task
-def lint():
-	"""lint - check style with flake8."""
-	run('flake8 collections_extended tests')
-
-
-@task
-def coverage():
-	"""coverage - check code coverage quickly with the default Python."""
-	run('coverage run --source collections_extended setup.py test')
-	run('coverage report -m')
-	run('coverage html')
-	run('open htmlcov/index.html')
-
-	log.info('collected test coverage stats')
-
-
 @task(clean)
 def publish():
 	"""publish - package and upload a release to the cheeseshop."""
@@ -56,13 +27,3 @@ def publish():
 	run('python setup.py bdist_wheel upload', pty=True)
 
 	log.info('published new release')
-
-
-@task
-def docs():
-	run('rm -f docs/collections_extended.rst')
-	run('rm -f docs/modules.rst')
-	run('sphinx-apidoc -o docs/ collections_extended')
-	run('make -C docs clean')
-	run('make -C docs html')
-	run('open docs/_build/html/index.html')
